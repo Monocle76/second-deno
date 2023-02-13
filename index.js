4e5a123a50309cba83ad6a36a570d90c7436757b
Monocle76 <monocle76@proton.me>
2023-02-13 11:49:40 +0530

    added html page route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { join } from "https://deno.land/std@0.152.0/path/mod.ts";
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 const app = new Application();
@@ -11,8 +12,12 @@ router
   })
   .get("/greet/:name", (c) => {
     c.response.body = { message: `Hello, ${c.params.name}` }
-  });
+  })
+  .get("/htmlpage", (c) => {
+    const htmlData = Deno.readTextFileSync(join(Deno.cwd(), "views", "index.html"));
+    c.response.body = htmlData
+  })
 
 app
   .use(router.routes(), router.allowedMethods())
-  .listen({ port: 80 });
+  .listen({ port: 3000 });
